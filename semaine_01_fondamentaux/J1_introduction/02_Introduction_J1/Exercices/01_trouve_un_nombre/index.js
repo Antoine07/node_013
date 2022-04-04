e237a3531ad428b79d012aee381b2c4d0656cc26
Antoine07 <antoine.lucsko@gmail.com>
2022-04-04 11:35:55 +0200

    exercices

---
+++ semaine_01_fondamentaux/J1_introduction/02_Introduction_J1/Exercices/01_trouve_un_nombre/index.js
@@ -0,0 +1,31 @@
+let count = 0;
+
+const searchNumber =  Math.floor( Math.random() * 100 ) + 1; ;
+
+console.log(
+  "Vous devez commencer le jeu choisissez un nombre compris entre 1 à 100"
+);
+
+process.stdin.on("data", (chunk) => {
+  const number = parseInt(chunk);
+
+  if (isNaN(number) === true) {
+    console.log("ce n'est pas un nombre");
+  }
+
+  if (count > 5) {
+    console.log(`Vous avez dépasser les ${5} tentatives`);
+    process.exit(0);
+  }
+
+  if (number > searchNumber) {
+    console.log(`Le nombre est plus petit que ${number}`);
+  } else if (number < searchNumber) {
+    console.log(`Le nombre est plus grand que ${number}`);
+  } else {
+    console.log(
+      `Vous avez gagnez en ${count}, c'était bien le nombre ${searchNumber}`
+    );
+    process.exit(0);
+  }
+});
