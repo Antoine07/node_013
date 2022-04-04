f42ac79df29f9169f098ef9b8c6b4a514780200d
Antoine07 <antoine.lucsko@gmail.com>
2022-04-04 15:31:36 +0200

    correction students

--- semaine_01_fondamentaux/J1_introduction/02_Introduction_J1/Exercices/02_Exercice_students/index.js
+++ semaine_01_fondamentaux/J1_introduction/02_Introduction_J1/Exercices/02_Exercice_students/index.js
@@ -1,8 +1,65 @@
-const fs = require('fs');
+const { readFileSync, appendFileSync } = require('fs');
 
-try {
-  const data = fs.readFileSync('./Data/students.txt', 'utf8')
-  console.log(data)
-} catch (err) {
-  console.error(err)
-};
+let students = [];
+
+function readStudents() {
+
+    try {
+        let data = readFileSync('./Data/students.txt', 'utf8');
+        data = data.split(/\r?\n/);
+
+        // dépile le premier élément 
+        const line = data.shift();
+
+        for (const st of data) {
+            const [note, name, address] = st.split(' ');
+            students.push({ note, name, address })
+        }
+
+        console.log(students);
+
+    } catch (err) {
+        console.error(err)
+    };
+
+}
+
+readStudents();
+
+// 2.Plus de 17 de moyenne
+
+const studentsBest = students.filter(s => s.note > 17);
+
+console.log(studentsBest);
+
+// 3. L'étudiant qui a eu la meilleur note
+
+// première solution 
+const studentBest = students.find(s => s.note == Math.max(...students.map(s => s.note)));
+console.log(studentBest);
+
+// deuxième solution
+let stBest = { name: null, note: 0, address: null };
+for (const st of students) {
+    if (st.note > stBest.note) stBest = st;
+}
+console.log(stBest);
+
+students.sort((s1, s2) => s1.note - s2.note)
+
+console.log(students);
+
+// appen
+
+appendFileSync('./Data/students.txt', "\n" + '18 Sonia Paris');
+appendFileSync('./Data/students.txt', "\n" + '17 Clarisse Marseille');
+
+readStudents();
+
+// Lire le fichier lui-même et mettez chaque nom en majuscule
+
+for( st of students){
+    st.name = st.name.toUpperCase();
+}
+
+console.log(students);
